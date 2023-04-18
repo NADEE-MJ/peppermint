88fa66281cf923601b7d0542044fc561094b448f
Steven <st3v3n110801>
2023-04-18 23:41:16 +0000

    reusable-table-draft

---
+++ frontend/src/lib/components/tables.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+
+import { Table } from '@skeletonlabs/skeleton';
+import type { TableSource } from '@skeletonlabs/skeleton';
+import { tableMapperValues } from '@skeletonlabs/skeleton';
+
+export let sourceData: Array<{}>;
+export let headerName: Array<string>;
+export let count: 
+
+</script>
+
+
+<div class="table-container">
+	<table class="table table-hover">
+		<thead>
+			<tr>
+				{#each headerName as column, i}
+					<th>{headerName[i]}</th>
+				{/each}
+            </tr>
+		</thead>
+		<tbody>
+            {#each sourceData as data, i}
+				<tr>
+					{#each sourceData[i] as column, i}
+						<td>{column}</td>
+					{/each}
+				</tr>
+			{/each}
+		</tbody>
+		<tfoot>
+			<tr>
+			</tr>
+		</tfoot>
+	</table>
+</div>
