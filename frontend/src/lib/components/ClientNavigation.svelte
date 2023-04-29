585fa5574f53a9ec9cb8441e2330db570710e728
NADEE-MJ <nadeem.maida@gmail.com>
2023-04-28 22:29:58 -0700

    consolidation of similar files

--- frontend/src/lib/components/ClientNavigation.svelte
+++
@@ -1,8 +0,0 @@
-<nav class="list-nav p-4">
-	<ul>
-		<li><a href="/client/upload-csv-test">Upload CSV Test</a></li>
-		<li><a href="/client/accounts">Accounts</a></li>
-		<li><a href="/client/budget/mine">Budget</a></li>
-		<li><a href="/client/skelly">Skelly</a></li>
-	</ul>
-</nav>
