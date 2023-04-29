5be35f956375ef0809f0c30953a4ed7ffd5180d9
NADEE-MJ <nadeem.maida@gmail.com>
2023-04-28 20:59:01 -0700

    cleanup

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -6,6 +6,7 @@
 		<header class="card-header text-center">
 			<h1 class="text-xl">Temporary Landing Page</h1>
 		</header>
+
 		<div class="p-4 flex justify-center">
 			<a href="/login" class="btn btn-xl variant-filled-primary card-hover">Login</a>
 		</div>
