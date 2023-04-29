9889d0cf5bf58e7969229dd9f0e96d901411885a
NADEE-MJ <nadeem.maida@gmail.com>
2023-04-28 20:56:50 -0700

    update client navigation

--- frontend/src/lib/components/ClientNavigation.svelte
+++ frontend/src/lib/components/ClientNavigation.svelte
@@ -4,5 +4,6 @@
 		<li><a href="/client/accounts">Accounts</a></li>
 		<li><a href="/client/budget/mine">Budget</a></li>
 		<li><a href="/client/skelly">Skelly</a></li>
+		<li><a href="/client/transactions/budget/1"><transactions-test></transactions-test></a></li>
 	</ul>
 </nav>
