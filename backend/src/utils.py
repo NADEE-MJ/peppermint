ed806d0a0749cedc5f9aa96b65a2aeceae64c665
Nadeem Maida <84353378+NADEE-MJ@users.noreply.github.com>
2023-05-07 22:48:57 -0700

    #95 emails not sending (#104)
    * changed to html and smtplib
    
    * update templates
    
    * fix all email templates and functions
    
    * linted files

--- backend/src/utils.py
+++ backend/src/utils.py
@@ -1,10 +1,12 @@
 import logging
+import smtplib
 from datetime import datetime, timedelta
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from pathlib import Path
-from typing import Any, Dict, Optional
+from typing import Optional
 
-import emails  # type: ignore
-from emails.template import JinjaTemplate  # type: ignore
+from jinja2 import Template
 from jose import JWTError, jwt
 from src.core.config import settings
 
@@ -12,74 +14,56 @@ from src.core.config import settings
 def send_email(
     email_to: str,
     subject_template: str = "",
-    html_template: str = "",
-    environment: Dict[str, Any] = {},
+    html_content: str = "",
 ) -> None:
     assert settings.EMAILS_ENABLED, "no provided configuration for email variables"
-    message = emails.Message(
-        subject=JinjaTemplate(subject_template),
-        html=JinjaTemplate(html_template),
-        mail_from=(settings.EMAILS_FROM_NAME, settings.EMAILS_FROM_EMAIL),
-    )
-    smtp_options = {"host": settings.SMTP_HOST, "port": settings.SMTP_PORT}
-    if settings.SMTP_USER:
-        smtp_options["user"] = settings.SMTP_USER
-    if settings.SMTP_PASSWORD:
-        smtp_options["password"] = settings.SMTP_PASSWORD
-    response = message.send(to=email_to, render=environment, smtp=smtp_options)
-    logging.info(f"send email result: {response}")
+    msg = MIMEMultipart()
+    msg["From"] = settings.EMAILS_FROM_EMAIL
+    msg["To"] = email_to
+    msg["Subject"] = subject_template
+    msg.attach(MIMEText(html_content, "html"))
+    with smtplib.SMTP(settings.SMTP_HOST, settings.SMTP_PORT) as smtp:
+        smtp.starttls()
+        smtp.login(settings.SMTP_USER, settings.SMTP_PASSWORD)
+        response = smtp.sendmail(msg["From"], msg["To"], msg.as_string())
+        # ! need to log responses to a file
+        logging.info(response)
 
 
 def send_test_email(email_to: str) -> None:
     project_name = settings.PROJECT_NAME
     subject = f"{project_name} - Test email"
-    with open(Path(settings.EMAIL_TEMPLATES_DIR) / "test_email.mjml") as f:
-        template_str = f.read()
-    send_email(
-        email_to=email_to,
-        subject_template=subject,
-        html_template=template_str,
-        environment={"project_name": settings.PROJECT_NAME, "email": email_to},
-    )
+    with open(Path(settings.EMAIL_TEMPLATES_DIR) / "test_email.html") as f:
+        template = Template(f.read())
+
+    html_content = template.render(project_name=project_name, email=email_to)
+    send_email(email_to=email_to, subject_template=subject, html_content=html_content)
 
 
 def send_new_account_email(email: str, password: str) -> None:
     project_name = settings.PROJECT_NAME
     subject = f"{project_name} - New account for user {email}"
-    with open(Path(settings.EMAIL_TEMPLATES_DIR) / "new_account.mjml") as f:
-        template_str = f.read()
-    link = settings.SERVER_HOST
-    send_email(
-        email_to=email,
-        subject_template=subject,
-        html_template=template_str,
-        environment={
-            "project_name": settings.PROJECT_NAME,
-            "password": password,
-            "email": email,
-            "link": link,
-        },
-    )
+    with open(Path(settings.EMAIL_TEMPLATES_DIR) / "new_account.html") as f:
+        template = Template(f.read())
+
+    link = f"{settings.SERVER_HOST}/login"
+    html_content = template.render(project_name=project_name, username=email, password=password, link=link)
+    send_email(email_to=email, subject_template=subject, html_content=html_content)
 
 
 def send_magic_link_email(email: str, token: str) -> None:
     project_name = settings.PROJECT_NAME
     subject = f"{project_name} - Magic Link {email}"
-    with open(Path(settings.EMAIL_TEMPLATES_DIR) / "magic_link.mjml") as f:
-        template_str = f.read()
+    with open(Path(settings.EMAIL_TEMPLATES_DIR) / "magic_link.html") as f:
+        template = Template(f.read())
+
     server_host = settings.SERVER_HOST
     link = f"{server_host}/magic-link?token={token}"
-    send_email(
-        email_to=email,
-        subject_template=subject,
-        html_template=template_str,
-        environment={
-            "project_name": settings.PROJECT_NAME,
-            "email": email,
-            "valid_hours": settings.EMAIL_RESET_TOKEN_EXPIRE_HOURS,
-            "link": link,
-        },
+
+    html_content = template.render(
+        project_name=project_name, email=email, valid_hours=settings.EMAIL_RESET_TOKEN_EXPIRE_HOURS, link=link
     )
+    send_email(email_to=email, subject_template=subject, html_content=html_content)
 
 
 def generate_magic_link_token(email: str) -> str:
